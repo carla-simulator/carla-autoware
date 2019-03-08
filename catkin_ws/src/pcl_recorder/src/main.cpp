5770494149f17f307475499245b1aae783197175
Pasch, Frederik <frederik.pasch@intel.com>
2019-03-08 16:18:50 +0100

    Cleanup license headers

--- catkin_ws/src/pcl_recorder/src/main.cpp
+++ catkin_ws/src/pcl_recorder/src/main.cpp
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2019 Intel Labs.
+ * Copyright (c) 2019 Intel Corporation
  *
- * authors: Frederik Pasch
+ * This work is licensed under the terms of the MIT license.
+ * For a copy, see <https://opensource.org/licenses/MIT>.
  */
-
 #include <ros/ros.h>
 #include "PclRecorder.h"
 
