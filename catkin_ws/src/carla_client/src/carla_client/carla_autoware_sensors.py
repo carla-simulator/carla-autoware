3162f5055ee19f894a2f7f7d879454e729f297e4
Pasch, Frederik <frederik.pasch@intel.com>
2019-03-27 10:19:53 +0100

    Read Collisions and Lane Invasions from ROS

--- catkin_ws/src/carla_client/src/carla_client/carla_autoware_sensors.py
+++ catkin_ws/src/carla_client/src/carla_client/carla_autoware_sensors.py
@@ -36,6 +36,9 @@ class GnssSensor(object):
         bp = world.get_blueprint_library().find('sensor.other.gnss')
         self.sensor = world.spawn_actor(bp, carla.Transform(carla.Location(x=1.0, z=2.8)), attach_to=self._parent)
 
+    def __del__(self):
+        self.sensor.destroy()
+
 # ==============================================================================
 # -- LidarSensor ---------------------------------------------------------------
 # ==============================================================================
@@ -55,6 +58,9 @@ class LidarSensor(object):
         bp.set_attribute('rotation_frequency', '20')
         self.sensor = world.spawn_actor(bp, carla.Transform(carla.Location(x=0.0, z=2.4)), attach_to=self._parent)
 
+    def __del__(self):
+        self.sensor.destroy()
+
 # ==============================================================================
 # -- FrontCameraSensor ---------------------------------------------------------
 # ==============================================================================
@@ -67,6 +73,9 @@ class FrontCamera(object):
         bp = world.get_blueprint_library().find('sensor.camera.rgb')
         bp.set_attribute('role_name', 'front')
         self.sensor = world.spawn_actor(bp, carla.Transform(carla.Location(x=2.0, z=2.0)), attach_to=self._parent)
+
+    def __del__(self):
+        self.sensor.destroy()
 
 # ==============================================================================
 # -- CameraSensor for Visualization --------------------------------------------
@@ -82,3 +91,36 @@ class ViewCamera(object):
         bp.set_attribute('image_size_x', '800')
         bp.set_attribute('image_size_y', '600')
         self.sensor = world.spawn_actor(bp, carla.Transform(carla.Location(x=-5.5, z=2.8), carla.Rotation(pitch=-15)), attach_to=self._parent)
+
+    def __del__(self):
+        self.sensor.destroy()
+
+# ==============================================================================
+# -- CollisionSensor -----------------------------------------------------------
+# ==============================================================================
+
+class CollisionSensor(object):
+    def __init__(self, parent_actor):
+        self.sensor = None
+        self._parent = parent_actor
+        world = self._parent.get_world()
+        bp = world.get_blueprint_library().find('sensor.other.collision')
+        self.sensor = world.spawn_actor(bp, carla.Transform(), attach_to=self._parent)
+
+    def __del__(self):
+        self.sensor.destroy()
+
+# ==============================================================================
+# -- LaneInvasionSensor --------------------------------------------------------
+# ==============================================================================
+
+class LaneInvasionSensor(object):
+    def __init__(self, parent_actor):
+        self.sensor = None
+        self._parent = parent_actor
+        world = self._parent.get_world()
+        bp = world.get_blueprint_library().find('sensor.other.lane_invasion')
+        self.sensor = world.spawn_actor(bp, carla.Transform(), attach_to=self._parent)
+
+    def __del__(self):
+        self.sensor.destroy()
