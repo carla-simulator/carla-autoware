0322a49f524911f48465566733d24504a65bb54b
Pasch, Frederik <frederik.pasch@intel.com>
2020-04-21 09:14:29 +0200

    Remove code, update doc

--- catkin_ws/check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-autopep8 src/carla_autoware_bridge/src/carla_autoware_bridge/* --in-place --max-line-length=100
-autopep8 src/carla_autoware_ego_vehicle/src/carla_autoware_ego_vehicle/* --in-place --max-line-length=100
-autopep8 src/carla_points_map_loader/src/carla_points_map_loader/* --in-place --max-line-length=100
-
-FILES="$(ls src/carla_autoware_bridge/src/carla_autoware_bridge/*) $(ls src/carla_autoware_ego_vehicle/src/carla_autoware_ego_vehicle/*)  $(ls src/carla_points_map_loader/src/carla_points_map_loader/*)"
-
-pylint --rcfile=.pylintrc $FILES
