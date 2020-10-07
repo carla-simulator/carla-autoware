fbdd75a71d60098fc86072472ebcbe3039663e03
Joel Moriana <joel.moriana@gmail.com>
2020-10-07 14:08:45 +0200

    run.sh script updated

--- run.sh
+++ run.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-XSOCK=/tmp/.X11-unix
-XAUTH=$HOME/.Xauthority
-
-VOLUMES="--volume=$XSOCK:$XSOCK:rw
-         --volume=$XAUTH:$XAUTH:rw
-         --volume=$(pwd)/autoware-contents:/home/autoware/autoware-contents:ro"
-
 RUNTIME=""
 DOCKER_VERSION=$(docker version --format '{{.Client.Version}}' | cut --delimiter=. --fields=1,2)
 if [[ $DOCKER_VERSION < "19.03" ]] && ! type nvidia-docker; then
@@ -17,8 +10,7 @@ fi
 
 docker run \
     -it --rm \
-    $VOLUMES \
-    --env="XAUTHORITY=${XAUTH}" \
+    --volume=$(pwd)/autoware-contents:/home/autoware/autoware-contents:ro \
     --env="DISPLAY=${DISPLAY}" \
     --privileged \
     --net=host \
