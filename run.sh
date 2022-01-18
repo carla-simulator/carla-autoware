e08b856d99ea1d26e4a5484c7c5f050c179a14b4
Peng Xu <robotpengxu@gmail.com>
2022-01-17 22:07:34 -0800

    Update code and patch to work with Carla 0.9.11.

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Default settings
+USER_ID="$(id -u)"
+XAUTH=$HOME/.Xauthority
+
 RUNTIME=""
 DOCKER_VERSION=$(docker version --format '{{.Client.Version}}' | cut --delimiter=. --fields=1,2)
 if [[ $DOCKER_VERSION < "19.03" ]] && ! type nvidia-docker; then
@@ -10,10 +14,11 @@ fi
 
 docker run \
     -it --rm \
-    --volume=$(pwd)/autoware-contents:/home/autoware/autoware-contents:ro \
+    --volume=$(pwd)/autoware-contents:/home/autoware/autoware-contents:rw \
+    --env="XAUTHORITY=${XAUTH}" \
     --env="DISPLAY=${DISPLAY}" \
+    --env="USER_ID=$USER_ID" \
     --privileged \
     --net=host \
     $RUNTIME \
     carla-autoware:latest
-
